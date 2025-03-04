0b6671e4536ad6e42ecc7fe1388db748f36a6f78
Joanna Fornal <fornal.asia@gmail.com>
2025-03-04 21:56:08 +0100

    Initial commit

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://86d614be21436bb1bf1918cccca05244@o4508920533876736.ingest.de.sentry.io/4508920544362576",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
---
+++ src/app/global-error.tsx
@@ -0,0 +1,51 @@
+// "use client";
+
+// import * as Sentry from "@sentry/nextjs";
+// import NextError from "next/error";
+// import { useEffect } from "react";
+
+// export default function GlobalError({
+//    error }: {
+//      error: Error & { digest?: string }
+//      }) {
+//   useEffect(() => {
+//     Sentry.captureException(error);
+//   }, [error]);
+
+//   return (
+//     <html>
+//       <body>
+//         {/* `NextError` is the default Next.js error page component. Its type
+//         definition requires a `statusCode` prop. However, since the App Router
+//         does not expose status codes for errors, we simply pass 0 to render a
+//         generic error message. */}
+//         <NextError statusCode={0} />
+//       </body>
+//     </html>
+//   );
+// }
+
+"use client"; // Error boundaries must be Client Components
+import { useEffect } from "react";
+import * as Sentry from "@sentry/nextjs";
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+  return (
+    // global-error must include html and body tags
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  );
+}
